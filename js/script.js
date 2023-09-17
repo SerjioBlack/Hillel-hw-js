9d7adcafd20058ec40105d31c06a9c55f45fb2f4
SerjioBlack <serjioblack@gmail.com>
2023-09-17 06:57:05 +0300

    HW-4 completed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 'use strict';
 
-let number1 = prompt('Add first number', '2');
-let number2 = prompt('Add second number', '3');
-alert( "Result: " + (parseInt(number1) + parseInt(number2)));
-console.log("Result: " + (parseInt(number1) - parseInt(number2)));
-console.log("Result: " + (parseInt(number1) * parseInt(number2)));
-console.log("Result: " + (parseInt(number1) / parseInt(number2)));
+function Add(number1, number2){
+    return number1 + number2;
+}
+
+function Sub(number1, number2){
+    return number1 - number2;
+}
+
+function Mul(number1, number2){
+    return number1 * number2;
+}
+
+function Div(number1, number2){
+    return number1 / number2;
+}
+
+let number1 = parseInt(prompt('Add first number'));
+let number2 = parseInt(prompt('Add second number'));
+
+console.log( "Sub: " + Sub(number1, number2));
+console.log( "Add: " + Add(number1, number2));
+console.log( "Mul: " + Mul(number1, number2));
+console.log( "Div: " + Div(number1, number2));
