3530c89cad005efb920d1f4b8f969174702c6e2d
SerjioBlack <serjioblack@gmail.com>
2023-09-17 05:25:37 +0300

    HW-2 completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 'use strict';
 
-let user= prompt('What is your name?', 'Alex');
-alert('Hello, ' + user + '! How are you?');
+
+//Task #1
+
+let name = "John";
+let admin = name;
+alert(admin);
+
+//Task #2
+
+let ourPlanet = "Earth";
+let userName = "John";
+
+//Task #3
+
+let sum = '10' + 10;
+console.log(typeof sum);
+
+
+
+
+
+
