41fbb4f733cde2555522e2a584144d6fa399e84e
SerjioBlack <serjioblack@gmail.com>
2023-10-02 12:35:55 +0300

    HW-8 completed

--- js/script.js
+++ js/script.js
@@ -1,40 +1,7 @@
 'use strict';
 
-// Task #1
-
-let user = {};
-
-user.name = 'John';
-user.surname = 'Smith';
-
-user.name = 'Pete';
-
-delete user.name;
-console.log(user);
-
-// Task #2
-
-const user_1 = {
-    name: "John"
-};
-// Це працюватиме?
-user_1.name = "Pete";
-
-//Ответ: Працюватиме.
-// Объявление const защищает только саму переменную от изменений.
-// user хранит ссылку на объект. И это не может быть изменено. Но содержимое объекта менять можно.
-
-// Task #3
-
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-};
-
-let sum = null;
-for (let key in salaries) {
-    sum += salaries[key];
-}
-
-console.log(sum);
+Boolean(Number('10')) + 1;//вернет 2
+'sub' + Number(false);//вернет "sub0";
+16 * '91'//вернет 1456;
+true-70//вернет -69;
+Number(1 + String(1)) + 1//вернет 12;
