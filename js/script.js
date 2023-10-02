987264ed6faf6b289a251502aa046d3ede2f3913
SerjioBlack <serjioblack@gmail.com>
2023-10-02 12:49:25 +0300

    HW-11 completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
-let hours = Number(prompt('enter the Hours value', ''));
-let sumSeconds = hours * 3600;
-alert(sumSeconds);
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+++num;
+--num;
+alert(num);
