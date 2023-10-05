732dd643c40edde5c1c3e3ad1239f240ca00ecea
SerjioBlack <serjioblack@gmail.com>
2023-10-05 16:35:35 +0300

    HW-14 completed

--- js/script.js
+++ js/script.js
@@ -1,59 +1,18 @@
 'use strict';
 
-let a = +prompt('enter number', '');
-a === 0 ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-a > 0 ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-a < 0 ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-a >= 0 ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-a <= 0 ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-a !== 0 ? console.log('Вірно') : console.log('Неправильно');
-
-let a = prompt('enter test', '');
-a === "test" ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-a === 1 ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-(a > 0 && a < 5) ? console.log('Вірно') : console.log('Неправильно');
-
-let a = +prompt('enter number', '');
-(a === 0 || a === 2) ? console.log(a+=7) : console.log(a/=10);
-
-let a = +prompt('enter number a', '');
-let b = +prompt('enter number b', '');
-(a <= 1 && b >= 3) ? console.log(a+b) : console.log(a-b);
-
-let a = +prompt('enter number a', '');
-let b = +prompt('enter number b', '');
-(a > 2 && a < 11) || (b >=6 && b < 14) ? console.log('Вірно') : console.log('Невірно');
+let numOrStr = prompt('input number or string');
 
 
-let num = 1;
-
-switch (num) {
-    case 1:
-        console.log('зима');
-        break;
-    case 2:
-        console.log('весна');
-        break;
-    case 3:
-        console.log('літо');
-        break;
-    case 4:
-        console.log('осінь');
-        break;
+switch (true) {
+    case (numOrStr === null):
+        console.log('ви скасували')
+        break
+    case (numOrStr.trim() === ''):
+        console.log('Empty String')
+        break
+    case isNaN( +numOrStr):
+        console.log(' number is Ba_NaN')
+        break
     default:
-        console.log("enter number from 1 to 4");
+        console.log('OK!')
 }
