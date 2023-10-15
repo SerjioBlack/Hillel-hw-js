dcbe30e0591fd5ec305905e4262d07bfe8dd375e
SerjioBlack <serjioblack@gmail.com>
2023-10-15 18:36:29 +0300

    HW-22 completed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 'use strict;'
 
-let result = 0;
+const string = prompt("enter something");
+const targetLength = prompt("enter length");
+const direction = prompt("left or right", "left");
+const symb = prompt("enter symbol");
 
-function numToExponent(
-    num = +prompt("Enter number"),
-    exponent = +prompt("Enter exponent")
-) {
-    if (isNaN(num, exponent)) {
-        alert("its not a number");
-    } else {
-        result = num ** exponent;
+function padString(string, targetLength) {
+    if (string !== null && targetLength !== null && direction === "left") {
+        return string
+            .substr(0, targetLength)
+            .toString()
+            .padStart(targetLength, symb);
+    } else if (
+        string !== null &&
+        targetLength !== null &&
+        direction === "right"
+    ) {
+        return string.toString().padEnd(targetLength, symb);
+    } else if (string === null || targetLength !== null || direction !== null) {
+        return "Check all fields";
     }
-    return result;
 }
-numToExponent();
-alert(result);
+alert(padString(string, targetLength));
