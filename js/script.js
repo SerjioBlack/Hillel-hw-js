e689374150962b7f1a069ac4c4702eab355d3ef6
SerjioBlack <serjioblack@gmail.com>
2023-10-15 18:38:57 +0300

    HW-20 completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
 function generateKey(length, characters) {
-  let key = '';
+  let key = "";
   for (i = 0; i <= length - 1; i++) {
-    const index = Math.floor(Math.random() * characters.length);
+    let index = Math.floor(Math.random() * characters.length);
     key += characters[index];
   }
   return key;
 }
-const key = generateKey(8, characters);
+let key = generateKey(8, characters);
 console.log(key);
