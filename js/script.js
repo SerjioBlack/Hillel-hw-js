8e105026bb7f5e86b76cbae89624890c79fab8ac
SerjioBlack <serjioblack@gmail.com>
2023-10-15 18:34:20 +0300

    HW-21 completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-'use strict';
+'use strict;'
 
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-function generateKey(length, characters) {
-  let key = '';
-  for (i = 0; i <= length - 1; i++) {
-    const index = Math.floor(Math.random() * characters.length);
-    key += characters[index];
-  }
-  return key;
+let result = 0;
+
+function numToExponent(
+    num = +prompt("Enter number"),
+    exponent = +prompt("Enter exponent")
+) {
+    if (isNaN(num, exponent)) {
+        alert("its not a number");
+    } else {
+        result = num ** exponent;
+    }
+    return result;
 }
-const key = generateKey(8, characters);
-console.log(key);
+numToExponent();
+alert(result);
