5ac5b1ea64e063e5c22d3f44a6151b56d839f2f2
SerjioBlack <serjioblack@gmail.com>
2023-10-20 11:50:08 +0300

    HW-24 completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
 'use strict';
 
-const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-function generateKey(length, characters) {
-  let key = "";
-  for (i = 0; i <= length - 1; i++) {
-    let index = Math.floor(Math.random() * characters.length);
-    key += characters[index];
+const mainFunction = (callback) => {
+  const number = parseFloat(prompt("Введіть число:"));
+  const exponent = parseFloat(prompt("Введіть ступінь:"));
+
+  if (!isNaN(number) && !isNaN(exponent)) {
+    callback(number, exponent);
+  } else {
+    alert("Будь ласка, введіть коректні числа.");
   }
-  return key;
-}
-let key = generateKey(8, characters);
-console.log(key);
+};
+
+const exponentiation = (number, exponent) => {
+  const result = Math.pow(number, exponent);
+  alert(`Результат: ${number} в ступені ${exponent} дорівнює ${result}`);
+};
+mainFunction(exponentiation);
+
+
+//// Callback функція multiply
+
+const multiplyFunction = (callback) => {
+  const firstNumber = parseFloat(prompt("Введіть перше число:"));
+  const secondNumber = parseFloat(prompt("Введіть друге число:"));
+
+  if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+    callback(firstNumber, secondNumber);
+  } else {
+    alert("Будь ласка, введіть коректні числа.");
+  }
+};
+
+const multiplyCallback = (firstNumber, secondNumber) => {
+  const result = firstNumber * secondNumber;
+  alert(
+      `Результат: ${firstNumber} помножене на ${secondNumber} дорівнює ${result}`
+  );
+};
+multiplyFunction(multiplyCallback);
