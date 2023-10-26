0ebc19151c24a3ba955c4e2294286c5ab01d6174
SerjioBlack <serjioblack@gmail.com>
2023-10-26 11:03:51 +0300

    HW-25 completed

--- js/script.js
+++ js/script.js
@@ -1,40 +1,10 @@
 'use strict';
 
-const mainFunction = (callback) => {
-  const number = parseFloat(prompt("Введіть число:"));
-  const exponent = parseFloat(prompt("Введіть ступінь:"));
-
-  if (!isNaN(number) && !isNaN(exponent)) {
-    callback(number, exponent);
-  } else {
-    alert("Будь ласка, введіть коректні числа.");
-  }
-};
-
-const exponentiation = (number, exponent) => {
-  const result = Math.pow(number, exponent);
-  alert(`Результат: ${number} в ступені ${exponent} дорівнює ${result}`);
-};
-mainFunction(exponentiation);
-
-
-//// Callback функція multiply
-
-const multiplyFunction = (callback) => {
-  const firstNumber = parseFloat(prompt("Введіть перше число:"));
-  const secondNumber = parseFloat(prompt("Введіть друге число:"));
-
-  if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
-    callback(firstNumber, secondNumber);
-  } else {
-    alert("Будь ласка, введіть коректні числа.");
-  }
-};
-
-const multiplyCallback = (firstNumber, secondNumber) => {
-  const result = firstNumber * secondNumber;
-  alert(
-      `Результат: ${firstNumber} помножене на ${secondNumber} дорівнює ${result}`
-  );
-};
-multiplyFunction(multiplyCallback);
+let arr = prompt("введите значения через запятую, минимум 4 значения").split(
+    ","
+);
+console.log(arr);
+arr.sort((a, b) => a - b);
+console.log(arr);
+arr.splice(1, 3);
+console.log(arr);
