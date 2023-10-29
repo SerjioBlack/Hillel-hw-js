7e112ccdee70d89f67d3969e1d4d3644aeadc8eb
SerjioBlack <serjioblack@gmail.com>
2023-10-29 13:17:30 +0200

    HW-28 completed

--- js/script.js
+++ js/script.js
@@ -1,69 +1,65 @@
 'use strict';
 
-let users = [
-    {
-        "index": 0,
-        "isActive": true,
-        "balance": "$2,226.60",
-        "name": "Eugenia Sawyer",
-        "gender": "female",
-        "phone": "+1 (840) 583-3207",
-        "address": "949 John Street, Rose, Puerto Rico, 1857"
-    },
-    {
-        "index": 1,
-        "isActive": true,
-        "balance": "$2,613.77",
-        "name": "Pauline Gallegos",
-        "gender": "female",
-        "phone": "+1 (985) 593-3328",
-        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-    },
-    {
-        "index": 2,
-        "isActive": false,
-        "balance": "$3,976.41",
-        "name": "Middleton Chaney",
-        "gender": "male",
-        "phone": "+1 (995) 591-2478",
-        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-        "index": 3,
-        "isActive": true,
-        "balance": "$1,934.58",
-        "name": "Burns Poole",
-        "gender": "male",
-        "phone": "+1 (885) 559-3422",
-        "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-        "index": 4,
-        "isActive": true,
-        "balance": "$3,261.65",
-        "name": "Mcfadden Horne",
-        "gender": "male",
-        "phone": "+1 (942) 565-3988",
-        "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-        "index": 5,
-        "isActive": false,
-        "balance": "$1,790.56",
-        "name": "Suzette Lewis",
-        "gender": "female",
-        "phone": "+1 (837) 586-3283",
-        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+let arr = ["apple", "John", "51", "Interstellar", "276"]
+const indexOfNew = (arr, item, from = 0) => {
+    for (let i = from; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return i;
+        }
     }
-]
+    return -1;
+};
 
-const reachUsers = users.filter((users) => users["balance"] >= "$2,000");
-const reachUsersPhones = reachUsers.map((reachUsers) => reachUsers["phone"]);
-console.log(reachUsersPhones);
+const lastIndexOf = (arr, item, from = arr.length - 1) => {
+    for (let i = from; i < arr.length; i--) {
+        if (arr[i] === item) {
+            return i;
+        }
+    }
+    return -1;
+};
 
+const find = (arr, item, index) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return item;
+        }
+    }
+    return undefined;
+};
 
+const findIndex = (arr, item, index) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return i;
+        }
+    }
+    return -1;
+};
 
-const sum = users.map((users) => users["balance"].slice(1));
-const balances = sum.map((string) => +string.replace(/[\s,]/g, ""));
-const total = balances.reduce((sum, current) => sum + current);
-console.log(total.toPrecision(7));
+const includes = (arr, item, from = 0) => {
+    for (let i = from; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const everyLengthSearch = (arr, maxLength, minLength) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length >= maxLength && arr[i].length <= minLength) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const someLengthSearch = (arr, maxLength, minLength) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length >= maxLength || arr[i].length <= minLength) {
+            return true;
+        }
+    }
+    return false;
+};
