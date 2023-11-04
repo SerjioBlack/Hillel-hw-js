fe2a2a81c67425d818a1f0f8d0b161f765428c36
SerjioBlack <serjioblack@gmail.com>
2023-11-04 12:27:53 +0200

    HW-30 completed

--- js/script.js
+++ js/script.js
@@ -1,65 +1,22 @@
 'use strict';
 
-let arr = ["apple", "John", "51", "Interstellar", "276"]
-const indexOfNew = (arr, item, from = 0) => {
-    for (let i = from; i < arr.length; i++) {
-        if (arr[i] === item) {
-            return i;
-        }
-    }
-    return -1;
-};
+const arr = [1, 2, 3, 4, {}, [1, 2, 3], [1, 2, 3], [3, 4, [5, 6, 7]]];
 
-const lastIndexOf = (arr, item, from = arr.length - 1) => {
-    for (let i = from; i < arr.length; i--) {
-        if (arr[i] === item) {
-            return i;
-        }
-    }
-    return -1;
-};
+function outerFlat(arr) {
+    let res = [];
 
-const find = (arr, item, index) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === item) {
-            return item;
+    function flat(subArr) {
+        for (let i = 0; i < subArr.length; i++) {
+            if (Array.isArray(subArr[i])) {
+                flat(subArr[i]);
+            } else {
+                res.push(subArr[i]);
+            }
         }
     }
-    return undefined;
-};
+    flat(arr);
+    return res;
+}
 
-const findIndex = (arr, item, index) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === item) {
-            return i;
-        }
-    }
-    return -1;
-};
+const flated = outerFlat(arr);
 
-const includes = (arr, item, from = 0) => {
-    for (let i = from; i < arr.length; i++) {
-        if (arr[i] === item) {
-            return true;
-        }
-    }
-    return false;
-};
-
-const everyLengthSearch = (arr, maxLength, minLength) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].length >= maxLength && arr[i].length <= minLength) {
-            return true;
-        }
-    }
-    return false;
-};
-
-const someLengthSearch = (arr, maxLength, minLength) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].length >= maxLength || arr[i].length <= minLength) {
-            return true;
-        }
-    }
-    return false;
-};
--- readme.md
+++ readme.md
@@ -1,7 +1,30 @@
-Вам потрібно реалізувати функціїindexOf,lastIndexOf,find,findIndex,includes,everyтаsome,вони працюють так само, як одноіменні методи масивів.
+Дописати функцію flat так щоб вона повертала правильне значення
 
-Алгоритм виконання:
+const arr = [
+1, 2, 3, 4, {},
+[1, 2, 3],
+[1, 2, 3],
+[3, 4,
+[5, 6, 7]
+]
+]
 
-Читайте документацію по методу над яким працюєте
-Робираєтесь, як працює метод і що повертає
-пишете функцію, яка повністю імітує роботу методу
+
+
+function outerFlat() {
+let res = []
+return function flat(arr) {
+for(let i = 0; i < arr.length; i++) {
+if(Array.isArray(arr[i])) {
+flat(arr[i], res)
+} else {
+res.push(arr[i])
+}
+}
+return res;
+}
+}
+
+const flat = outerFlat()
+console.log(flat(arr));
+console.log(flat(arr));
