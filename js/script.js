80f119b319b27f031dee965120abf42f24e37751
SerjioBlack <serjioblack@gmail.com>
2023-11-07 08:30:28 +0200

    HW-31 completed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,65 @@
 'use strict';
 
-let getFactorial = (n) => {
-    return n === 1 ? n : n * getFactorial(n - 1);
-};
+function counter(initialValue = 0) {
+    let count = initialValue;
+    let increaseCount = 0;
+    let decreaseCount = 0;
+    let getCount = 0;
 
-let pow = (num, degree) => {
-    return degree === 1 ? num : num * pow(num, degree - 1);
-};
+    function increase() {
+        count++;
+        increaseCount++;
+    }
 
-let getSum = (a, b) => {
-    return b === 0 ? a : getSum(a + 1, b - 1);
-};
+    function decrease() {
+        count--;
+        decreaseCount++;
+    }
+
+    function value() {
+        getCount++;
+        return count;
+    }
+
+    function getStatistic() {
+        return {
+            increaseCount,
+            decreaseCount,
+            getCount,
+        };
+    }
+
+    function reset() {
+        count = initialValue;
+        increaseCount = 0;
+        decreaseCount = 0;
+        getCount = 0;
+    }
+
+    return {
+        increase,
+        decrease,
+        value,
+        getStatistic,
+        reset,
+    };
+}
+
+const counter1 = counter(0);
+const counter2 = counter(10);
+
+counter1.increase();
+counter1.increase();
+counter1.decrease();
+counter2.decrease();
+counter2.increase();
+counter2.decrease();
+
+counter1.getStatistic();
+counter2.getStatistic();
+
+counter1.reset();
+counter2.reset();
+
 
 
