180f46da7281c3328fba603063a96e1e5446c522
SerjioBlack <serjioblack@gmail.com>
2023-11-10 10:42:15 +0200

    HW-32 completed

--- js/script.js
+++ js/script.js
@@ -1,65 +1,25 @@
 'use strict';
 
-function counter(initialValue = 0) {
-    let count = initialValue;
-    let increaseCount = 0;
-    let decreaseCount = 0;
-    let getCount = 0;
+const user = {
+    name: "Serjio",
+};
 
-    function increase() {
-        count++;
-        increaseCount++;
-    }
+function info(phone, email) {
+    return `${this.name}, ${phone}, ${email}`;
+}
 
-    function decrease() {
-        count--;
-        decreaseCount++;
-    }
-
-    function value() {
-        getCount++;
-        return count;
-    }
-
-    function getStatistic() {
-        return {
-            increaseCount,
-            decreaseCount,
-            getCount,
-        };
-    }
-
-    function reset() {
-        count = initialValue;
-        increaseCount = 0;
-        decreaseCount = 0;
-        getCount = 0;
-    }
-
-    return {
-        increase,
-        decrease,
-        value,
-        getStatistic,
-        reset,
+function bind(fn, context, ...rest) {
+    return function (...args) {
+        const someKey = "value";
+        context[someKey] = fn;
+        const res = context[someKey](...rest.concat(args));
+        delete context[someKey];
+        return res;
     };
 }
 
-const counter1 = counter(0);
-const counter2 = counter(10);
+bind(info, user, "0800500500", "1@gmail.com")();
 
-counter1.increase();
-counter1.increase();
-counter1.decrease();
-counter2.decrease();
-counter2.increase();
-counter2.decrease();
-
-counter1.getStatistic();
-counter2.getStatistic();
-
-counter1.reset();
-counter2.reset();
 
 
 
