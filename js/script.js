6dd25af45e02716701d09d87e35bcbae344fe8a6
SerjioBlack <serjioblack@gmail.com>
2023-11-13 16:25:59 +0200

    HW-32 completed

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ function info(phone, email) {
     return `${this.name}, ${phone}, ${email}`;
 }
 
-function bind(fn, context, ...rest) {
+(function bind(fn, context, ...rest) {
     return function (...args) {
         const someKey = "value";
         context[someKey] = fn;
         const res = context[someKey](...rest.concat(args));
         delete context[someKey];
         return res;
-    };
-}
+    }
+})();
 
 bind(info, user, "0800500500", "1@gmail.com")();
 
