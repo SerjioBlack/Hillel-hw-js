64afa9d749cb90bf45d95b16a797579f8acb7452
SerjioBlack <serjioblack@gmail.com>
2023-12-04 15:35:57 +0200

    HW-43 completed

--- js/script.js
+++ js/script.js
@@ -1,57 +1,66 @@
 'use strict';
 
-(() => {
-  const tabs = document.querySelectorAll('.tab');
-  const tabContents = document.querySelectorAll('.tab-content');
+(function () {
+  document.addEventListener('DOMContentLoaded', () => {
+    const favoritesList = document.getElementById('favoritesList');
+    const itemsList = document.getElementById('itemsList');
+    const colorSchemeInputs = document.querySelectorAll(
+      'input[name="colorScheme"]',
+    );
 
-  tabs.forEach((tab) => {
-    tab.addEventListener('click', function () {
-      tabContents.forEach((tabContent) => (tabContent.style.display = 'none'));
-
-      const tabId = this.id.replace('tab', 'content');
-
-      const selectedTab = document.getElementById(tabId);
-      selectedTab.style.display = 'block';
-    });
-  });
-})();
-
-(() => {
-  const displayedImage = document.querySelector('.displayed-img');
-  const thumbBar = document.querySelector('.thumb-bar');
-  const btn = document.querySelector('button');
-  const overlay = document.querySelector('.overlay');
-  const full = document.querySelector('.full-img');
-  const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-
-  for (const image of images) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', `image/${image}`);
-    thumbBar.appendChild(newImage);
-    newImage.addEventListener('click', (e) => {
-      displayedImage.src = e.target.src;
-    });
-  }
-
-  full.addEventListener('click', () => {
-    // При клике, масштабируем элемент
-    if (full.style.transform === 'scale(1)') {
-      full.style.transform = 'scale(1.2)';
-    } else {
-      full.style.transform = 'scale(1)';
+    const savedColorScheme = localStorage.getItem('colorScheme');
+    if (savedColorScheme) {
+      document.body.classList.add(savedColorScheme);
+      colorSchemeInputs.forEach((input) => {
+        if (input.value === savedColorScheme) {
+          input.checked = true;
+        }
+      });
     }
-  });
 
-  btn.addEventListener('click', () => {
-    const btnClass = btn.getAttribute('class');
-    if (btnClass === 'dark') {
-      btn.setAttribute('class', 'light');
-      btn.innerHTML = 'Lighten';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } else {
-      btn.setAttribute('class', 'dark');
-      btn.innerHTML = 'Darker';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    const savedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    function updateFavorites() {
+      favoritesList.innerHTML = '';
+      savedFavorites.forEach((itemId) => {
+        const item = document.querySelector(`[data-id="${itemId}"]`);
+        if (item) {
+          const li = document.createElement('li');
+          li.textContent = item.textContent;
+          favoritesList.appendChild(li);
+        }
+      });
     }
+
+    function toggleFavorite(itemId) {
+      const index = savedFavorites.indexOf(itemId);
+      if (index === -1) {
+        savedFavorites.push(itemId);
+      } else {
+        savedFavorites.splice(index, 1);
+      }
+      localStorage.setItem('favorites', JSON.stringify(savedFavorites));
+      updateFavorites();
+    }
+
+    function applyColorScheme(scheme) {
+      document.body.className = scheme;
+      localStorage.setItem('colorScheme', scheme);
+    }
+
+    colorSchemeInputs.forEach((input) => {
+      input.addEventListener('change', function () {
+        applyColorScheme(this.value);
+      });
+    });
+
+    itemsList.addEventListener('click', (e) => {
+      if (e.target.classList.contains('toggleFavorite')) {
+        const itemId = e.target.closest('li').dataset.id;
+        toggleFavorite(itemId);
+      }
+    });
+
+    updateFavorites();
   });
-})();
+}());
