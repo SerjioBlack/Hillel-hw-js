b36da712b237a8013bb8992cfc478e29c1df59b8
SerjioBlack <serjioblack@gmail.com>
2024-02-08 14:02:40 +0200

    HW-67 completed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
-'use strict';
+'use strict;'
+function cacheComplexCalculations(complexFunction) {
+  const cache = new WeakMap();
 
-function analyzeText(text) {
-  const cleanedText = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').toLowerCase();
-
-  const words = cleanedText.split(/\s+/);
-
-  const uniqueWordsSet = new Set(words);
-
-  const uniqueWordsArray = Array.from(uniqueWordsSet);
-
-  return {
-    uniqueWordsCount: uniqueWordsArray.length,
-    uniqueWords: uniqueWordsArray
+  return function (obj) {
+    if (cache.has(obj)) {
+      console.log("Retrieving result from cache...");
+      return cache.get(obj);
+    } else {
+      console.log("Calculating result...");
+      const result = complexFunction(obj);
+      cache.set(obj, result);
+      return result;
+    }
   };
 }
 
-const text = "Це - рядок тексту. Рядок має унікальні слова: привіт, Привіт, світ, світ.";
-const result = analyzeText(text);
-console.log(result.uniqueWordsCount);
-console.log(result.uniqueWords);
+function complexFunction(obj) {
+  return obj.value * obj.value;
+}
+
+const cachedComplexFunction = cacheComplexCalculations(complexFunction);
+
+const obj1 = { value: 5 };
+const obj2 = { value: 10 };
+
+console.log(cachedComplexFunction(obj1));
+console.log(cachedComplexFunction(obj2));
+console.log(cachedComplexFunction(obj2));
