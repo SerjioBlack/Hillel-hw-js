afd8a8e9d2b18ef6a16af8886da1da446d230a39
SerjioBlack <serjioblack@gmail.com>
2024-02-08 13:57:45 +0200

    HW-65 completed

--- js/script.js
+++ js/script.js
@@ -1,44 +1,57 @@
-'use strict';
-
-(() => {
-  function Human(name, age) {
-    this.name = name;
-    this.age = age;
+class TaskManager {
+  constructor() {
+    this.tasks = new Map();
   }
 
-  Human.prototype.displayInfo = function () {
-    console.log(`Name: ${this.name}, Age: ${this.age}`);
-  };
-
-  function Car(brand, model, year, licensePlate, owner) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.licensePlate = licensePlate;
-
-    if (owner instanceof Human && owner.age >= 18) {
-      this.owner = owner;
+  addTask(id, description) {
+    if (this.tasks.has(id)) {
+      console.log('Помилка: Завдання з таким ID вже існує.');
     } else {
-      console.log("Invalid owner or owner is under 18 years old.");
+      this.tasks.set(id, description);
+      console.log('Завдання успішно додано.');
     }
   }
 
-  Car.prototype.displayInfo = function () {
-    console.log(`Car: ${this.brand} ${this.model}, Year: ${this.year},
-License Plate: ${this.licensePlate}`);
-    if (this.owner) {
-      console.log("Owner Info:");
-      this.owner.displayInfo();
+  removeTask(id) {
+    if (this.tasks.has(id)) {
+      this.tasks.delete(id);
+      console.log('Завдання успішно видалено.');
+    } else {
+      console.log('Помилка: Завдання з таким ID не існує.');
     }
-  };
+  }
 
-  const person1 = new Human("Vova", 25);
-  const person2 = new Human("Vasya", 17);
+  findTask(id) {
+    if (this.tasks.has(id)) {
+      return this.tasks.get(id);
+    }
+    return 'Помилка: Завдання не знайдено.';
+  }
 
-  const car1 = new Car("Toyota", "Camry", 2020, "ABC123", person1);
-  const car2 = new Car("Honda", "Civic", 2018, "XYZ789", person2);
+  displayTasks() {
+    console.log('Список завдань:');
+    this.tasks.forEach((description, id) => {
+      console.log(`ID: ${id}, Опис: ${description}`);
+    });
+  }
 
-  car1.displayInfo();
-  car2.displayInfo();
+  updateTaskDescription(id, newDescription) {
+    if (this.tasks.has(id)) {
+      this.tasks.set(id, newDescription);
+      console.log('Опис завдання успішно оновлено.');
+    } else {
+      console.log('Помилка: Завдання з таким ID не існує.');
+    }
+  }
+}
 
-})();
+const manager = new TaskManager();
+
+manager.addTask(1, 'Зробити покупки');
+manager.addTask(2, 'Відправити листа');
+console.log(manager.findTask(1));
+
+manager.updateTaskDescription(1, 'Зробити покупки в супермаркеті');
+manager.displayTasks();
+manager.removeTask(2);
+manager.displayTasks();
