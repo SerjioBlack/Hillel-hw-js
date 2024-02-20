beb0523a509d33ca76dd3d2a40651abd1b16e044
SerjioBlack <serjioblack@gmail.com>
2024-02-20 16:55:17 +0200

    HW-70 completed

--- js/script.js
+++ js/script.js
@@ -1,59 +1,36 @@
-'use strict;'
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = "5d066958a60d315387d9492393935c19";
+    const city = "DNIPRO";
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
 
-function searchPost() {
-  const postId = document.getElementById("postIdInput").value;
-  if (postId < 1 || postId > 100) {
-    alert("ID повинно бути від 1 до 100");
-    return;
-  }
+    const weatherIcon = document.getElementById("weatherIcon");
+    const cityElement = document.getElementById("city");
+    const temperatureElement = document.getElementById("temperature");
+    const pressureElement = document.getElementById("pressure");
+    const descriptionElement = document.getElementById("description");
+    const humidityElement = document.getElementById("humidity");
+    const windSpeedElement = document.getElementById("windSpeed");
+    const windDirectionElement = document.getElementById("windDirection");
 
-  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((post) => {
-        const postContainer = document.getElementById("postContainer");
-        postContainer.innerHTML = `
-        <h2>Знайдений пост:</h2>
-        <div>
-          <h3>${post.title}</h3>
-          <p>${post.body}</p>
-          <button onclick="fetchComments(${postId})">Отримати
-коментарі</button>
-        </div>
-      `;
-      })
-      .catch((error) => {
-        alert("При пошуку поста виникла помилка:", error.message);
-        console.error("Error:", error);
-      });
-}
-
-function fetchComments(postId) {
-  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((comments) => {
-        const postContainer = document.getElementById("postContainer");
-        const commentsList = comments
-            .map((comment) => `<li>${comment.body}</li>`)
-            .join("");
-        postContainer.innerHTML += `
-        <h3>Коментарі:</h3>
-        <ul>${commentsList}</ul>
-      `;
-      })
-      .catch((error) => {
-        alert("При отриманні коментарів виникла помилка:", error.message);
-        console.error("Error:", error);
-      });
-}
-
-
+    fetch(apiUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            cityElement.textContent = data.name;
+            temperatureElement.textContent = data.main.temp;
+            pressureElement.textContent = data.main.pressure;
+            descriptionElement.textContent = data.weather[0].description;
+            humidityElement.textContent = data.main.humidity;
+            windSpeedElement.textContent = data.wind.speed;
+            windDirectionElement.textContent = data.wind.deg;
+            weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        })
+        .catch((error) => {
+            console.error("There was a problem fetching the weather data:", error);
+        });
+});
