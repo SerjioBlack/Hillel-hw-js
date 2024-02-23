c1a2b8a27572a591efca2c69ad198ec441704575
SerjioBlack <serjioblack@gmail.com>
2024-02-23 12:40:29 +0200

    HW-74 completed

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+      <div className="App">
+        <Card title="Title 1" />
+
+        <Card text="Text 2" />
+
+        <Card title="Title 3" text="Text 3" />
+      </div>
+  );
+}
+
+export default App;
