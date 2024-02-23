eb4d9d440ec5182b49b40c1635bd33fac6289506
SerjioBlack <serjioblack@gmail.com>
2024-02-23 12:44:45 +0200

    HW-73 completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,10 @@
 import Card from "./components/Card";
 
 function App() {
-  return (
-      <div className="App">
-        <Card title="Title 1" />
-
-        <Card text="Text 2" />
-
-        <Card title="Title 3" text="Text 3" />
-      </div>
-  );
+    return (
+        <div className="App">
+            <Card />
+        </div>
+    );
 }
-
 export default App;
--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const Card = ({title, text}) => {
-    return (
-        <div className="card">
+class Card extends React.Component {
+
+    render() {
+        return <div className="card">
             <div className="card-body">
-                {title && <h4 className="card-title">{title}</h4>}
-                {text && <p className="card-text">{text}</p>}
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some quick example text to
+                    build on the card</p>
+                <button type="button" className="btn btn-primary">Go
+                    somewhere</button>
             </div>
         </div>
-    );
+    }
 }
 
 export default Card;
